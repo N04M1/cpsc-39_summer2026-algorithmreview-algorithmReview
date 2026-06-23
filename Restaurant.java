b17f81b8998c91dbd9fdd77c145850c6d41f49f6
M0AR0SE <digitalkilljoy@gmail.com>
2026-06-23 02:43:39 +0000

    Add Restaurant.java

---
+++ Restaurant.java
@@ -0,0 +1,56 @@
+// Created by Francisco
+import.java.util.ArrayList;
+
+public class Restaurant {
+
+    private String name;
+    private double rating;
+    private ArrayList<MenuItem> menuItems;
+
+    //Default constructor
+    public Restaurant() {
+        this.name = "Unknown";
+        this.rating = 0.0;
+        this.menuItems = new ArrayList<>();
+        
+    }
+
+    //Paramterized constructor
+    public Restaurant(String name, double rating){
+        this.name = name;
+        this.rating = rating;
+        this.menuItems = new ArrayList<>();
+    }
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public Void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public ArrayList<MenuItem> getMenuItems() {
+        return menuItems;
+    }
+
+    //Add a menu item to the restaurant
+    public void addMenuItem(MenuItem item) {
+        menuItems.add(item);
+    }
+
+    //toString Method
+    public String toString() {
+        return name + " (Rating: " + rating + ")";
+    }
+    
+}
